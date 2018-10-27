02449f382eb0dd9fef7433b5bb6790b2fff49e97
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-27 13:19:28 +0200

    remove redundant code

--- App.js
+++ App.js
@@ -17,20 +17,21 @@ $.ajaxSetup({
 //	return str;
 // }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('To do');
-var doingColumn = new Column('Doing');
-var doneColumn = new Column('Done');
+// to tez juz nie bedzie potrzebne
+//TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
+//var todoColumn = new Column('To do');
+//var doingColumn = new Column('Doing');
+//var doneColumn = new Column('Done');
 
 // DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
+//board.createColumn(todoColumn);
+//board.createColumn(doingColumn);
+//board.createColumn(doneColumn);
 
 // TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
+//var card1 = new Card('Nowe zadanie');
+//var card2 = new Card('stworzyc tablice kanban');
 
 // DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
+//todoColumn.createCard(card1);
+//doingColumn.createCard(card2);
