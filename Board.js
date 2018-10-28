7a8ec0cd3048250de3da67b8ec208f097d6d060c
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-28 15:05:40 +0100

    Update Board.js

--- Board.js
+++ Board.js
@@ -7,9 +7,20 @@ var board = {
 	element: $('#board .column-container')
 };
 
-$('.create-column')
-	.click(function(){
-		board.createColumn(new Column(prompt('Wpisz nazwę kolumny')));
+$('.create-column').click(function(){
+		var columnName = prompt('Wpisz nazwę kolumny');
+		board.createColumn(new Column(columnName));
+		$.ajax({
+			url: baseUrl + '/column',
+			method: 'POST',
+			data: {
+			name: columnName
+			},
+			succes: function(response){
+				var column = new Column(response.id, columnName);
+				board.create.Column(column);
+			}
+		});
 	});
 	
 function initSortable() {
