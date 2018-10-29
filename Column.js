8d86c94a53bd984d7c8b7ba44c0b01b3a7000b58
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-29 12:04:53 +0100

    Update Column.js

--- Column.js
+++ Column.js
@@ -4,34 +4,38 @@ function Column(id, name) {
 	
 	this.id = id;
 	this.name = name;
-	this.element = createColumn();
+	this.$element = createColumn();
 
 	function createColumn() {
 		// TWORZENIE NOWYCH WĘZŁÓW
-		var column = $('<div class="column"></div>');
-		var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
-		var columnCardList = $('<ul class="card-list"></ul>');
-		var columnDelete = $('<button class="btn-delete">x</button>');
-		var columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
+		var $column = $('<div class="column"></div>');
+		var $columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
+		var $columnCardList = $('<ul class="card-list"></ul>');
+		var $columnDelete = $('<button class="btn-delete">x</button>');
+		var $columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
 		
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
-		column.append(columnTitle)
-			.append(columnDelete)
-			.append(columnAddCard)
-			.append(columnCardList);
-			return column;
+		 $column.append( $columnTitle)
+			.append( $columnDelete)
+			.append( $columnAddCard)
+			.append( $columnCardList);
+			return  $column;
 		}
 	}
 Column.prototype = {
 	createCard: function(card) {
-	  this.element.children('ul').append(card.element);
+	  this. $element.children('ul').append( $card.element);
 	},
 	deleteColumn: function() {
 	   var self = this;
 	   $.ajax({
 		   url: baseUrl + '/.column/' + self.id,
 		   method: 'DELETE',
-		   
-	  //this.element.remove();
-	})
-}};
+		   success: function(response){
+		   self.$element.remove();
+     		   }
+   		});
+   	}
+};
+
+		 
